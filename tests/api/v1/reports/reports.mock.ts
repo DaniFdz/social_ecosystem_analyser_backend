7792104e3d2a079e0835bd06365ec3ffda97d1c2
MarioGamez <100451332@alumnos.uc3m.es>
2024-08-25 23:39:42 +0200

    Missing test changes

--- tests/api/v1/reports/reports.mock.ts
+++ tests/api/v1/reports/reports.mock.ts
@@ -1,13 +1,19 @@
-import { type GeneralReport } from '@v1/reports/models/reportsInterface'
+import { type ReportByTopic, type GeneralReport } from '@v1/reports/models/reportsInterface'
 import { type Status, type ReportsRepository } from '@v1/reports/repository/reportsRepository'
 import { jest } from '@jest/globals'
 
 export class MockReportsRepository implements ReportsRepository {
   data: GeneralReport[] = []
+  dataByTopic: ReportByTopic[] = []
 
   constructor () {
     this.data = []
   }
+
+  getReportByTopic: (topic: string) => Promise<ReportByTopic | null> = jest.fn(async (topic: string) => {
+    const report = this.dataByTopic.find((report) => report.topic === topic)
+    return report ?? null
+  })
 
   getReportById: (id: string) => Promise<GeneralReport | null> = jest.fn(async (id: string) => {
     const report = this.data.find((report) => report.id === id)
