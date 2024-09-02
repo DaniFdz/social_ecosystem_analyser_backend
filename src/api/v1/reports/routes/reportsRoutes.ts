6f37187330a12fa0bf8a6cfceca453df35c1c26b
MarioGamez <100451332@alumnos.uc3m.es>
2024-09-02 22:10:50 +0200

    change auth reports

--- src/api/v1/reports/routes/reportsRoutes.ts
+++ src/api/v1/reports/routes/reportsRoutes.ts
@@ -10,9 +10,9 @@ export function getReportsRouter (reportsRepository: ReportsRepository): Router 
   const reportsController = new ReportsController(reportsRepository)
 
   router
-    .use(noGuests)
     .get('/:id', async (req, res) => { await reportsController.getReports(req, res) })
     .get('/topic/:topic', async (req, res) => { await reportsController.getTopicReport(req, res) })
+    .use(noGuests)
     .post('/', async (req, res) => { await reportsController.addReport(req, res) })
   return router
 }
